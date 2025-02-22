256fb72ccfec305a72c6b9c3137315d4fc4dec40
Ying Xiang <f416720001@yahoo.com.tw>
2025-02-22 23:06:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+### Init
+
+```shell
+docker compose up --build
+```
+
+```shell
+❯ docker compose exec backend python manage.py createsuperuser
+Username (leave blank to use 'root'):
+Email address:
+Password:
+Password (again):
+Superuser created successfully.
+```
+
---
+++ backend/blog/models.py
@@ -0,0 +1,13 @@
+from django.db.models import CharField, TextField
+from django_extensions.db.models import TimeStampedModel
+
+
+class Post(TimeStampedModel):
+    title = CharField(max_length=200)
+    content = TextField()
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
